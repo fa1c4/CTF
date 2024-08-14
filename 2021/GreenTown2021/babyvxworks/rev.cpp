b90ce023539d19f9524b8424fe4bba758ba9ba1e
fa1c4 <957042361@qq.com>
2024-08-14 11:37:24 +0800

    update rev.cpp in GT2021 babyvxworks

--- 2021/GreenTown2021/babyvxworks/rev.cpp
+++ 2021/GreenTown2021/babyvxworks/rev.cpp
@@ -1,25 +1,26 @@
 #include <stdio.h>
 
 
-int checking[] = { 
+unsigned char checking[] = { 
                 188, 10, 187, 193, 213, 134, 127, 10, 
                 201, 185, 81, 78, 136, 10, 130, 185, 
                 49, 141, 10, 253, 201, 199, 127, 185, 
-                17, 78, 185, 232, 141, 87, 0
+                17, 78, 185, 232, 141, 87
             };
 
 
 int main() {
     for (int cnt = 0; cnt < 30; ++cnt) {
         for (int i = 0; i < 30; ++i) {
-            checking[i] -= 3;
-            checking[i] ^= 0x22;
+            checking[i] -= (unsigned char)3;
+            checking[i] ^= (unsigned char)0x22;
         }
     }
 
     for (int cnt = 0; cnt < 30; ++cnt) {
         printf("%c", checking[cnt]);
     }
+    printf("\n");
 
     return 0;
 }   
